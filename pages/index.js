c89f308114f834b2d109904b63e2ace7699aca33
Santiago Aguirre <58629459+santiagoaguirreg@users.noreply.github.com>
2023-07-26 15:54:47 -0500

    Pagina de inicio

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import Image from 'next/image'
-import Link from 'next/link'
 import Layout from '../components/layout'
+import Guitarra from '../components/guitarra'
+import styles from '../styles/grid.module.css'
+import Post from '../components/post'
 
-export default function Home() {
+export default function Home({guitarras, posts}) {
+
   return (
 <>
 
@@ -10,14 +13,60 @@ export default function Home() {
        title={'Inicio'}
        description={"Blog de música"}
       >
-      <h1>
-        Hola mundo
-      </h1>
+        <main className='contenedor'>
+          <h1 className='heading'>Nuestra Colección</h1>
 
-      <Link href="/nosotros"> Nosotros</Link>
+          <div className={styles.grid}>
+        {guitarras.map(guitarra => (
+          <Guitarra 
+            key={guitarra.id}
+            guitarra={guitarra.attributes}
+          />
 
+        ))}
+        </div>
+
+        </main>
+
+        <section className='contenedor'>
+          <h2 className='heading'>Blog</h2>
+          <div className={styles.grid}>
+            {posts.map(post => (
+              <Post 
+               key={post.id}
+               post={post.attributes}
+              />
+            ))}
+        </div>
+        </section>
+    
       </Layout>
 
  </>
   )
+}
+
+export async function getStaticProps() {
+  const urlGuitarras = `${process.env.API_URL}/guitarras?populate=imagen`
+  const urlPosts = `${process.env.API_URL}/posts?populate=imagen`
+
+
+  const [ resGuitarras, resPosts] = await Promise.all([
+    fetch(urlGuitarras),
+    fetch(urlPosts)
+  ])
+
+  const [{data: guitarras}, {data: posts}] = await Promise.all([
+    resGuitarras.json(),
+    resPosts.json()
+  ])
+
+
+  return {
+    props: {
+      guitarras,
+      posts
+    }
+  }
+
 }
